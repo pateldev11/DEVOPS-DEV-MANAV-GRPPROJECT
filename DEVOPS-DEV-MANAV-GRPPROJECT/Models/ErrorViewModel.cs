4bf1d69cb27f6dc5b8d79ea38fe793954f7651fe
Devkumar Patel <dpat1072@my.centennialcollege.ca>
2025-04-13 14:54:54 -0400

    first commit

---
+++ DEVOPS-DEV-MANAV-GRPPROJECT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DEVOPS_DEV_MANAV_GRPPROJECT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
